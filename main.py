bd1b5ddd892edcee3925a90d981b0843b7306300
gwdiii <georgedanforth@proton.me>
2023-04-18 17:56:34 -0500

    added npcs, paralax, collision

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 
+from random import randint
 from pygame.locals import (
     K_UP,
     K_DOWN,
@@ -19,28 +21,52 @@ class Player(pygame.sprite.Sprite):
         super(Player, self).__init__()
         self.dimensions = (25, 25)
         self.surface = pygame.Surface((self.dimensions))
-        self.rectangle = self.surface.get_rect()
+        self.rect = self.surface.get_rect()
         self.surface.fill(WHITE)
         self.center = (WINDOW[0] - self.dimensions[0] >> 1,
                        WINDOW[1] - self.dimensions[1] >> 1)
 
     def update(self, keys):
-        if keys[K_UP]:    self.rectangle.move_ip( 0,-5)
-        if keys[K_DOWN]:  self.rectangle.move_ip( 0, 5)
-        if keys[K_LEFT]:  self.rectangle.move_ip(-5, 0)
-        if keys[K_RIGHT]: self.rectangle.move_ip( 5, 0)
+        if keys[K_UP]:    self.rect.move_ip( 0,-5)
+        if keys[K_DOWN]:  self.rect.move_ip( 0, 5)
+        if keys[K_LEFT]:  self.rect.move_ip(-5, 0)
+        if keys[K_RIGHT]: self.rect.move_ip( 5, 0)
 
-        if self.rectangle.top  <= 0: self.rectangle.top  = 0
-        if self.rectangle.left <  0: self.rectangle.left = 0
-        if self.rectangle.bottom >= WINDOW[1]: self.rectangle.bottom = WINDOW[1]
-        if self.rectangle.right  >  WINDOW[0]: self.rectangle.right  = WINDOW[0]
+        if self.rect.top  <= 0: self.rect.top  = 0
+        if self.rect.left <  0: self.rect.left = 0
+        if self.rect.bottom >= WINDOW[1]: self.rect.bottom = WINDOW[1]
+        if self.rect.right  >  WINDOW[0]: self.rect.right  = WINDOW[0]
+
+class NPC(pygame.sprite.Sprite):
+    def __init__(self):
+        super(NPC, self).__init__()
+        self.dimensions = (3, 3)
+        self.surface = pygame.Surface((self.dimensions))
+        self.rect = self.surface.get_rect(
+            center = (
+                randint(WINDOW[0] + self.dimensions[0], WINDOW[0] + 100),
+                randint(0, WINDOW[1])
+            )
+        )
+        self.surface.fill(WHITE)
+        self.speed = randint(1, 3)
+ 
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0: self.kill()
 
 pygame.init()
 
-screen = pygame.display.set_mode(WINDOW)
-player = Player()
-running = True
+screen  = pygame.display.set_mode(WINDOW)
+add_npc = pygame.USEREVENT + 1
+pygame.time.set_timer(add_npc, 100)
 
+player  = Player()
+npcs    = pygame.sprite.Group()
+sprites = pygame.sprite.Group()
+sprites.add(player)
+
+running = True
 while running:
 
     for event in pygame.event.get():
@@ -49,13 +75,23 @@ while running:
 
         if event.type == QUIT: running = False
 
+        if event.type == add_npc:
+            new_npc = NPC()
+            npcs.add(new_npc)
+            sprites.add(new_npc)
+
     keys = pygame.key.get_pressed()
 
     player.update(keys)
+    npcs.update()
 
     screen.fill(BLACK)
 
-    screen.blit(player.surface, player.rectangle)
+    for sprite in sprites: screen.blit(sprite.surface, sprite.rect)
+
+    # if pygame.sprite.spritecollideany(player, npcs):
+    #     player.kill()
+    #     running = False
 
     pygame.display.flip()
 
